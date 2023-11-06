e8fb0b5ba5fa0044c5217c454f2cc58598dbea68
REValdiviezo <ezequielrvaldiviezo@gmail.com>
2023-11-06 19:10:44 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Titulo from "./components/Titulo";
+
+function App() {
+  return (
+    <>
+    <Titulo/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,9 @@
+const Titulo = () => {
+    return(
+        <div>
+            <h1 className="text-blue-500 text-xl">hola mundo</h1>
+        </div>
+    )
+}
+
+export default Titulo
