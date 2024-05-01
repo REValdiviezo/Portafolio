aa47b0aa8cf415f5e393b9436e80c8624511b8f6
REValdiviezo <ezequielrvaldiviezo@gmail.com>
2024-05-01 04:13:37 -0300

    Modificacion del componente Tecnologias

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import { BrowserRouter as Router } from 'react-router-dom';
-import NavBar from "./components/NavBar";
-import Titulo from "./components/Titulo";
-import SobreMi from "./components/SobreMi";
-import Proyectos from "./components/Proyectos";
+import NavBar from './components/NavBar';
+import Titulo from './components/Titulo';
+import SobreMi from './components/SobreMi';
+import Proyectos from './components/Proyectos';
 import Contacto from './components/Contacto';
 import Tecnologias from './components/Tecnologias';
 import Footer from './components/Footer';
 
-
 function App() {
   return (
     <Router>
-        <NavBar />
-        <Titulo />
-        <Tecnologias/>
-        <SobreMi />
-        <Proyectos />
-        <Contacto/>
-        <Footer/>
+      <NavBar />
+      <Titulo />
+      <Tecnologias />
+      <SobreMi />
+      <Proyectos />
+      <Contacto />
+      <Footer />
     </Router>
   );
 }
--- src/components/Tecnologias.jsx
+++ src/components/Tecnologias.jsx
@@ -1,32 +1,53 @@
 import { useState, useEffect } from 'react';
-import imagen1 from "../assets/descarga.png";
-import imagen2 from "../assets/descarga2.png";
-import imagen3 from "../assets/descarga3.png";
+import html from "../assets/carrusel/HTML.png";
+import css from "../assets/carrusel/CSS.png";
+import javascript from "../assets/carrusel/Javascript.png";
+import express from "../assets/carrusel/express.png";
+import git from "../assets/carrusel/git.png";
+import github from "../assets/carrusel/github.png";
+import node from "../assets/carrusel/node.png";
+import postgre from "../assets/carrusel/postgre.png";
+import postman from "../assets/carrusel/postman.png";
+import react from "../assets/carrusel/React.png";
+import redux from "../assets/carrusel/redux.png";
+import sequelize from "../assets/carrusel/sequelize.png";
+import sql from "../assets/carrusel/sql.png";
+import tailwind from "../assets/carrusel/tailwind.png";
 
 const Tecnologias = () => {
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const images = [imagen1, imagen2, imagen3,imagen1, imagen2, imagen3,imagen1, imagen2, imagen3,imagen1, imagen2, imagen3]; // Array de imágenes
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
-    }, 2000); // Cambia 5000 por el tiempo deseado en milisegundos para cambiar de imagen automáticamente
-
-    return () => clearInterval(interval);
-  }, [currentIndex, images]);
 
   return (
-    <div className="relative w-[8px] h-[30px]">
-      <div className="flex absolute top-0 transition-transform duration-1000" style={{ width: `${images.length * 100}%`, transform: `translateX(-${currentIndex * (100 / images.length)}%)` }}>
-        {images.map((imageUrl, index) => (
-          <img
-            key={index}
-            src={imageUrl}
-            alt={`Slide ${index}`}
-            className="w-full h-full object-cover"
-          />
-        ))}
+    <div className="slider w-9/12 h-auto m-auto overflow-hidden">
+      <div className='slide-track flex '>
+        <div className='w-[200px]'><img className='w-full' src={tailwind} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={html} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={css} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={javascript} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={express} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={git} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={github} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={node} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={postgre} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={postman} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={react} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={redux} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={sequelize} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={sql} alt="" /></div>
+
+        <div className='w-[200px]'><img className='w-full' src={tailwind} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={html} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={css} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={javascript} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={express} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={git} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={github} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={node} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={postgre} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={postman} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={react} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={redux} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={sequelize} alt="" /></div>
+        <div className='w-[200px]'><img className='w-full' src={sql} alt="" /></div>
       </div>
     </div>
   );
